776386ee2428af810232d06385d957a93533eab6
Hari krishna <karanam30@gmail.com>
2024-04-21 20:22:11 +0530

    some-message

--- 3rd_Script.sh
+++ 3rd_Script.sh
@@ -2,7 +2,9 @@
 echo "please typr the input for the name from the keyboard"
 #read var1 
 #read var2
-read  "Prompt" var1 
-read   var2 
+read  -s "Prompt" var1 
+#read   var2 
 echo "printing hte value $var1 as the ldDddJDdLDD"
-echo "printing the value $var2 as the lafasfaldal"
+#echo "printing the value $var2 as the lafasfaldal"
+
+exit 0
