2d79089a0bd68be04824944fbbce9602f13b9cee
Hari krishna <karanam30@gmail.com>
2024-04-26 11:59:32 +0530

    normal message

--- 4th_Script_arrayInput.sh
+++ 4th_Script_arrayInput.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/bash
 
-read -a array -p "Enter the valus for array:"
+# read -a array -p "Enter the valus for array:"
 
-for i in ${array[*]}:
-do
-    echo -e "printing the input $i "
-done    
+# for i in ${array[*]}:
+# do
+#     echo -e "printing the input $i "
+# done    
+read -n 6 -p "Enter the name : " name
+echo -e "\nName : $name"
