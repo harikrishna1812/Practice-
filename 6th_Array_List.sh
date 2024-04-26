f20d8d92e15333308447b6974eb51f792348329d
Hari krishna <karanam30@gmail.com>
2024-04-26 22:24:39 +0530

    normal message

--- 6th_Array_List.sh
+++ 6th_Array_List.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/bash
-ARRAYNAME=(“man”,”bear”,”pig”,”dog”,”cat”,“sheep”)
-for i in $ARRAYNAME
-do
-    echo -e "\n $i \n"
-done
-
+# ARRAYNAME=(“man”,”bear”,”pig”,”dog”,”cat”,“sheep”)
+# for i in $ARRAYNAME
+# do
+#     echo -e "\n $i \n"
+# done
+ANIMALS=”man bear pig dog cat sheep”
+for ANIMAL in $ANIMALS
+  do
+    echo $ANIMAL
+  done
 exit 0
