33cbfec1dec54e41de553324401e800e7cfdb9e8
Hari krishna <karanam30@gmail.com>
2024-04-26 13:23:59 +0530

    normal message

--- 2nd_Script.sh
+++ 2nd_Script.sh
@@ -9,6 +9,7 @@ echo "$leader : This is the leader ship"
 
 read var1 var2 var3 var4 
 
-echo -e "the first var1 $var1 second var2: $var2 third var3 : $var3 fourth Var4 : $var4
+echo -e "the first var1 $var1 second var2: $var2 third var3 : $var3 fourth Var4 : $var4"
+
 
 exit 0
