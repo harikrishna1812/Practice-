0b2af7620d97be0e530e1400381e826206132b28
Hari krishna <karanam30@gmail.com>
2024-04-26 14:20:59 +0530

    normal message

--- 5th_CheckingFile_inpath.sh
+++ 5th_CheckingFile_inpath.sh
@@ -1,18 +1,16 @@
 #! /usr/bin/bash 
-
-
-
 read -p path "Enter the path for to check the folder"
+ech0 -e "path"
 
-read  folder
+folder=linux_shel_scripting
 
 if [ -d "$folder" ]
 then
     echo "directory exists"
 else
     echo "directory does not exist"
-    mkdir folder 
-	echo " now created the folder here "
+   # mkdir folder 
+	#echo " now created the folder here "
 fi
 
 exit 0
