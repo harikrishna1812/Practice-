65057b67588d996745db44b1ccabe6916cc4b2a1
Hari krishna <karanam30@gmail.com>
2024-04-26 12:03:16 +0530

    normal message

--- 4th_Script_arrayInput.sh
+++ 4th_Script_arrayInput.sh
@@ -1,10 +1,13 @@
 #! /usr/bin/bash
 
-# read -a array -p "Enter the valus for array:"
+read -a array -p "Enter the valus for array:"
 
-# for i in ${array[*]}:
-# do
-#     echo -e "printing the input $i "
-# done    
+for i in ${array[*]}:
+do
+    echo -e "printing the input $i "
+done    
 read -n 6 -p "Enter the name : " name
 echo -e "\nName : $name"
+
+HOSTNAME=$(hostname)
+echo “This script is running on $HOSTNAME”
