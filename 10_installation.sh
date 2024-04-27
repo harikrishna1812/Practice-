cddfc9f5b387eeef0691c2b69dc88147472495d4
Hari krishna <karanam30@gmail.com>
2024-04-27 17:23:28 +0530

    normal message

---
+++ 10_installation.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+#This program is related to installation of packages
+USER_ID=$(id -u)
+if[id ne 0]
+then 
+    echo "please change it in admin mode"
+else 
+    echo "You are in admin mode"
+dnf install mysql -y 
+echo "is it script proceeding"
