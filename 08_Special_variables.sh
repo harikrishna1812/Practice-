a75af10c7c1df67ecfe612022a8a2b4696cd5533
Hari krishna <karanam30@gmail.com>
2024-04-27 08:36:52 +0530

    normal message

---
+++ 08_Special_variables.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+echo -e "Printing the all values paasing the cmd:$@"
+
+echo -e "Prinitng the count how many valuse passed to the script:$#"
+
+
+echo -e "Printing the running script :$0"
+
+
+echo -e "printing the present working DIR:$pwd"
