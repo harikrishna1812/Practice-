35f1da084789e5cc0e4fe7de13472a8a07805992
Hari krishna <karanam30@gmail.com>
2024-05-06 14:20:12 +0530

    normal message

---
+++ calculator.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash 
+
+#This program is used for to write a program fro calculator
+read -p "Enter the first array:" @a
+
+read -p "Enter the second array:" @b
+
+concatenat=@a+@b
+echo "the concatenation is $concatenat"
