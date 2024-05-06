1b7f25cf93b6f95639f2c9b82df48bddefbe37aa
Hari krishna <karanam30@gmail.com>
2024-05-06 11:38:12 +0530

    normal message

--- print_file_information.sh
+++ print_file_information.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 
-Current_file_dat= ls -al &>>file.txt
+#Current_file_dat= ls -al # &>>file.txt
+ls -al 
 
-echo " folder contenst $Current_file_dat "
+#echo " folder contenst $Current_file_dat "
