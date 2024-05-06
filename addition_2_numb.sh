30f2043c3e30e12013d3059701f577404194e889
Hari krishna <karanam30@gmail.com>
2024-05-06 12:55:04 +0530

    normal message

--- addition_2_numb.sh
+++ addition_2_numb.sh
@@ -1,15 +1,10 @@
 #! /usr/bin/bash
 
-# read -p  "enter the first number a:"
-# read -p  "enter the first number b:"
+ read -p  "enter the first number a:"a
+ read -p  "enter the first number b:"b
 
-a=$1
-b=$2
+# a=$1
+# b=$2
 sum=$(($a+$b))
 echo "the taotal sum of a and b is : $sum "
-
-# # Calculate sum
-# sum=$(( $1 + $2 ))
-
-# # Display the result
-# echo "Sum is: $sum"
+exit 0
