5a1aa4641105bbe0b8e54a6e987f5834bbac12ac
Hari krishna <karanam30@gmail.com>
2024-06-07 12:43:27 +0530

    adding jenkins

---
+++ jenkins.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash 
+
+#installing the jenkins packages in Linux machine 
+
+echo " this is for the script for installation of jenkins"
+echo "this is for getting the current script PID:$$"
+
+#Step1 : Installing the java packages 
+# sudo yum install java-1.8.0-openjdk-devel
+sudo yum install java-1.8.0-openjdk-devel
+
+#Step2 : Add the jenkins repository to the yum file 
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+
+#Step3 : creating the jenkins key 
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+#Install Jenkins
+sudo yum install jenkins
+
+#Step 4: Start Jenkins
+sudo systemctl enable jenkins
+
+#Step 6: Access Jenkins
+#http://<your_server_IP>:8080
--- max_number.sh
+++ max_number.sh
@@ -1,5 +1,20 @@
 #! /usr/bin/bash 
 
-a=(1,10,20,13,26)
-max_nmum=$max(a[5])
-echo" printing the max number  $max_num "
+# a=(1,10,20,13,26)
+# max_nmum=$max(a[5])
+# echo" printing the max number  $max_num "
+echo "please enter the number of elements"
+read N
+
+
+i=1
+
+if $i -le $N
+then 
+    max=$if
+
+else 
+then 
+    do 
+        read i 
+        if ()
