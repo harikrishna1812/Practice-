790f26a0e92a4eac30c29e785b1b3e2d7c584be9
Hari krishna <{ID}+{username}@users.noreply.github.com>
2024-06-18 15:46:12 +0530

    creating folders

--- 11_folder_creation.sh
+++ 11_folder_creation.sh
@@ -1,4 +1,16 @@
 #moving to a particular folder 
 #reading the names for to create the folders 
 #CWD=PWD
-echo $PWD
+echo "enter the nums: "
+read num1 num2
+
+if ! [ "$num1" -lt 10 ] || ! [ "$num2" -lt 10 ]; then
+    echo "you weren't supposed to do that" >&2    # message send to stderr
+    exit 1    # exit with error status
+fi
+
+mkdir "$num1" || exit $?    # if mkdir fails, exit with its error status
+
+for (( counter=0; counter<$num2; counter++ )); do
+    echo "${num1}${num2}" > "${num1}/${num1}${num2}${counter}.txt"
+done
