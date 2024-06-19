9b85a6cfcad8f174d08416e911b3c602ab200b8b
Hari krishna <{ID}+{username}@users.noreply.github.com>
2024-06-19 06:14:19 +0530

    do function

--- 12_reading_data_array.sh
+++ 12_reading_data_array.sh
@@ -3,9 +3,9 @@ echo " This is the script for reading the data to a array"
  
 # Loop upto size of array
 # starting from index, i=0
-for (( counter=0; counter<5; counter++ ))
-    read array[*]
-    # To print index, ith
-    # element
-    echo ${arr[$counter]}
+while true; do
+  read -p "Folder name:" input
+  [[ -z "$input" ]] && break
+  mkdir -p "$input"
+done
      
