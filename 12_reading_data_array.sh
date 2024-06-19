9a84c735c766c7bb1d1b3a6935163b0db57893dd
Hari krishna <{ID}+{username}@users.noreply.github.com>
2024-06-19 06:12:26 +0530

    do function

--- 12_reading_data_array.sh
+++ 12_reading_data_array.sh
@@ -4,7 +4,7 @@ echo " This is the script for reading the data to a array"
 # Loop upto size of array
 # starting from index, i=0
 for (( counter=0; counter<5; counter++ ))
-    read array[$couner]
+    read array[*]
     # To print index, ith
     # element
     echo ${arr[$counter]}
